d954f7c5d037e9e9900da87d365fc8a7814a29d1
Yi Ru <46683634+Lewesyru@users.noreply.github.com>
2019-03-17 15:58:04 -0400

    Add files via upload

---
+++ scripts/Windows/setup.ps1
@@ -0,0 +1,10 @@
+Write-Host "Script created by Yi Ru.`n"
+Write-Host "Start writing file...`n"
+
+(Add-Content C:\Windows\System32\drivers\etc\hosts "`n") -and `
+(Add-Content C:\Windows\System32\drivers\etc\hosts "140.232.230.73		ghostrider") -and `
+(Write-Host "Done." -ForegroundColor green)
+if ($?) {
+	Write-Host "Permission denied. Please run again as administrator.`n" -ForegroundColor red
+}
+Pause
