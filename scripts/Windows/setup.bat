4b503a13326b0d7ee81444546e40215f12fea190
Lewesyru <46683634+Lewesyru@users.noreply.github.com>
2019-03-17 00:37:33 -0400

    Upload scripts

---
+++ scripts/Windows/setup.bat
@@ -0,0 +1,10 @@
+@echo off
+cls
+echo Script created by Yi Ru.
+echo.
+echo Start writing file...
+echo. >> C:\Windows\System32\drivers\etc\hosts
+echo 140.232.230.73		ghostrider>> C:\Windows\System32\drivers\etc\hosts
+echo.
+echo If it prompts Access is denied, please run again as administrator.
+echo. & pause
