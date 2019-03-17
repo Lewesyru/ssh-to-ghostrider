ff168c6effb03eef624ba0cdaf79edaacf69880f
Yi Ru <46683634+Lewesyru@users.noreply.github.com>
2019-03-17 16:01:57 -0400

    Update setup.bat

--- scripts/Windows/setup.bat
+++ scripts/Windows/setup.bat
@@ -1,15 +1,15 @@
 @echo off
 
-cls
 echo Script created by Yi Ru.
-
 echo.
 echo Start writing file...
 echo.
 
-(echo. >> C:\Windows\System32\drivers\etc\hosts &&^
+(
+echo. >> C:\Windows\System32\drivers\etc\hosts &&^
 echo 140.232.230.73		ghostrider>> C:\Windows\System32\drivers\etc\hosts &&^
-echo Done.) ||^
+echo Done.
+) ||^
 echo Please run again as administrator.
 
 echo. & pause
