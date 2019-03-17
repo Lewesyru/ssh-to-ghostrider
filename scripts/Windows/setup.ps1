41089c5232f04249f62a94b549ddc9a38a153555
Yi Ru <46683634+Lewesyru@users.noreply.github.com>
2019-03-17 16:01:03 -0400

    Backup file. Do not use this.

--- scripts/Windows/setup.ps1
+++ scripts/Windows/setup.ps1
@@ -1,10 +1,12 @@
+# This script is for backup purpose only.
+# Do not use it.
+
 Write-Host "Script created by Yi Ru.`n"
 Write-Host "Start writing file...`n"
 
 (Add-Content C:\Windows\System32\drivers\etc\hosts "`n") -and `
 (Add-Content C:\Windows\System32\drivers\etc\hosts "140.232.230.73		ghostrider") -and `
-(Write-Host "Done." -ForegroundColor green)
-if ($?) {
-	Write-Host "Permission denied. Please run again as administrator.`n" -ForegroundColor red
-}
+(Write-Host "Done." -ForegroundColor green) -or `
+(Write-Host "Permission denied. Please run again as administrator.`n" -ForegroundColor red)
+
 Pause
