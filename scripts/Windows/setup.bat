65a919a30f7d6edd622a8721555cadd3bfe810ca
Yi Ru <46683634+Lewesyru@users.noreply.github.com>
2019-03-17 15:10:58 -0400

    Update setup.bat

--- scripts/Windows/setup.bat
+++ scripts/Windows/setup.bat
@@ -1,10 +1,15 @@
 @echo off
+
 cls
 echo Script created by Yi Ru.
+
 echo.
 echo Start writing file...
-echo. >> C:\Windows\System32\drivers\etc\hosts
-echo 140.232.230.73		ghostrider>> C:\Windows\System32\drivers\etc\hosts
 echo.
-echo If it prompts Access is denied, please run again as administrator.
+
+(echo. >> C:\Windows\System32\drivers\etc\hosts &&^
+echo 140.232.230.73		ghostrider>> C:\Windows\System32\drivers\etc\hosts &&^
+echo Done.) ||^
+echo Please run again as administrator.
+
 echo. & pause
